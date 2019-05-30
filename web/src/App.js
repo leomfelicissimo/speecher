59c4453b11b70ffea4194e7229e785c5e7bdb292
Leonardo Felicissimo <leomfelicissimo@gmail.com>
2019-05-30 00:57:55 -0300

    Adding semantic ui to a more beautiful frontend

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Input, Image, Header, Icon } from 'semantic-ui-react';
 import styled from 'styled-components';
 import SpeecherApi from './services/SpeecherApi';
 
@@ -7,31 +8,21 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  width: 100%;
+  width: 50%;
   height: 100%;
 `;
 
-const Button = styled.button`
-  width: 80px;
-  font-size: 14px;
-`;
-
-const SentenceInput = styled.input`
-  height: 20px;
-  width: 50%;
-  padding: 10px;
+const SentenceInput = styled(Input)`
+  width: 100%;
   font-size: 14px;
   margin: 20px 0px;
 `;
 
-const Image = styled.img`
-  margin-top: 20px;
-`;
-
-function App() {
+const App = () => {
   const [sentence, setSentence] = useState('');
   const [imageUrl, setImageUrl] = useState('');
   const [search, setSearch] = useState('');
+  const KEY_ENTER = 'Enter';
 
   useEffect(() => {
     async function fetchData() {
@@ -41,18 +32,30 @@ function App() {
       setImageUrl(imageUrl);
     }
 
-    fetchData();
+    if (sentence) {
+      fetchData();
+    }
   }, [search]);
 
   return (
     <Container>
+      <Header as='h1' icon>
+        <Icon name="microphone" />
+        Speecher
+        <Header.Subheader>Enquanto você escreve (no futuro fala), as imagens aparecem</Header.Subheader>
+      </Header>
       <SentenceInput
         type="text"
+        icon="search"
+        placeholder="Digite uma frase..."
+        size="large"
         value={sentence}
         onChange={e => setSentence(e.target.value)}
+        onKeyUp={e => e.key === KEY_ENTER && setSearch(sentence)}
       />
-      <Button onClick={() => setSearch(sentence)}>Buscar</Button>
-      {imageUrl && <Image height="40%" src={imageUrl} alt="speecher image" /> }
+      {imageUrl && (
+        <Image src={imageUrl} alt="speecher image" fluid rounded />
+      )}
     </Container>
   );
 }
